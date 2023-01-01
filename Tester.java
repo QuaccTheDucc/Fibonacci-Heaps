9a8bc67e884b8a7e80a7804d41b64374ec8ec4ff
romtaugranot <romgranot@mail.tau.ac.il>
2023-01-01 13:40:07 +0200

    added a tester file, if needed add to your path the recommended JUnit.

---
+++ Tester.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+
+class Tester {
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void deleteMin() {
+    }
+
+    @Test
+    void findMin() {
+    }
+
+    @Test
+    void meld() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void getSentinel() {
+    }
+
+    @Test
+    void getMin() {
+    }
+
+    @Test
+    void getNumOfRoots() {
+    }
+}
